00d97c440a2abe5b66c36d74005fde32d4744422
dvk-fueled <dvk@fueled.co>
2013-12-16 16:08:46 +0530

    adding coffee

---
+++ coffee/todo.coffee
@@ -0,0 +1,36 @@
+app = angular.module 'Todo', ['Parse']
+
+app.config ( ParseProvider ) ->
+  ParseProvider.initialize(
+    "jml5ACSkQXYJDPFn7CxNof3wwn364cynInyohleY",
+    "AyAJDOV5RQD82r0HRb0Yp5DpBg0H5OFZ6H9rXmxM"
+    )
+
+app.controller 'TodoCtrl' = ($scope) ->
+  $scope.todos = [
+    text: "learning angular"
+    done: true
+  ,
+    text: "build an angular app"
+    done: false
+  ]
+  $scope.addTodo = ->
+    if $scope.todoText
+      $scope.todos.push
+        text: $scope.todoText
+        done: false
+
+      scope.todoText = ""
+
+  $scope.remaining = ->
+    count = 0
+    angular.forEach $scope.todos, (todo) ->
+      count += (if todo.done then 0 else 1)
+
+    count
+
+  $scope.archive = ->
+    oldTodos = $scope.todos
+    $scope.todos = []
+    angular.forEach oldTodos, (todo) ->
+      $scope.todos.push todo  unless todo.done
--- js/todo.js
+++ js/todo.js
@@ -1,28 +1,46 @@
-function TodoCtrl($scope){
-    $scope.todos = [
-    {text:'learning angular', done:true},
-    {text:'build an angular app', done:false}];
+var app;
 
-    $scope.addTodo = function() {
-        if ($scope.todoText) {
-            $scope.todos.push({text:$scope.todoText, done:false});
-            scope.todoText = '';
-        };
-    };
+app = angular.module('Todo', ['Parse']);
 
-    $scope.remaining = function() {
-        var count = 0;
-        angular.forEach($scope.todos, function(todo) {
-            count += todo.done ? 0 : 1;
-        });
-        return count;
-    };
+app.config(function(ParseProvider) {
+  return ParseProvider.initialize("jml5ACSkQXYJDPFn7CxNof3wwn364cynInyohleY", "AyAJDOV5RQD82r0HRb0Yp5DpBg0H5OFZ6H9rXmxM");
+});
 
-    $scope.archive = function() {
-        var oldTodos = $scope.todos;
-        $scope.todos = [];
-        angular.forEach(oldTodos, function(todo) {
-            if (!todo.done) $scope.todos.push(todo);
-        });
-    };
-}
+app.controller('TodoCtrl', function($scope) {
+  $scope.todos = [
+    {
+      text: "learning angular",
+      done: true
+    }, {
+      text: "build an angular app",
+      done: false
+    }
+  ];
+  $scope.addTodo = function() {
+    if ($scope.todoText) {
+      $scope.todos.push({
+        text: $scope.todoText,
+        done: false
+      });
+      return scope.todoText = "";
+    }
+  };
+  $scope.remaining = function() {
+    var count;
+    count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      return count += (todo.done ? 0 : 1);
+    });
+    return count;
+  };
+  return $scope.archive = function() {
+    var oldTodos;
+    oldTodos = $scope.todos;
+    $scope.todos = [];
+    return angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) {
+        return $scope.todos.push(todo);
+      }
+    });
+  };
+});
